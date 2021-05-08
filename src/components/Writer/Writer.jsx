058079ed534b642cb949ab2491180a4aa61b5fa4
SplinterDev <fabriciojcmontenegro@gmail.com>
2021-05-08 16:13:43 -0300

    Remove part of Writer section text

--- src/components/Writer/Writer.jsx
+++ src/components/Writer/Writer.jsx
@@ -4,7 +4,6 @@ function Writer() {
   return (
     <div className="Writer">
       <p>Although I'm Brazilian, I enjoy reading and writing fiction in English. You can check my stories on <a href="https://blog.reedsy.com/creative-writing-prompts/author/splinter-magus/" target="_blank" rel="noreferrer">Reedsy Prompts</a>, where my short story, <a href="https://blog.reedsy.com/creative-writing-prompts/contests/68/submissions/43670/" target="_blank" rel="noreferrer">Silence</a>, was picked among over a thousand submissions as the winner for contest #68 (I will talk about this at every opportunity I get).</p>
-      <p>I also sometimes post on <a href="https://fjcm.medium.com/" target="_blank" rel="noreferrer">Medium</a>, so if you like irregularly scheduled content, give it a follow. I have no schedule whatsoever, you're gonna love it!</p>
       <p>If you want to discuss what is the most compelling end-of-world scenario, you can add me on Discord (SplinterFM#6322) and we can throw some theories around. Also, if you're inclined, I can read your fiction for free and give you my humble opinion (be aware: I'm a slow reader).</p>
     </div>
   )
