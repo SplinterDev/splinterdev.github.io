97cca7884a2621940e2f9e9e2a17551af06b3481
SplinterDev <fabriciojcmontenegro@gmail.com>
2022-02-21 13:06:20 -0800

    Create nextjs project

---
+++ pages/developer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const Developer = (props: Props) => {
+  return <div>Developer</div>;
+};
+
+export default Developer;
--- src/components/Sidenav/Sidenav.tsx
+++ src/components/Sidenav/Sidenav.tsx
@@ -1,39 +1,76 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import './Sidenav.scss'
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React from 'react';
+import style from './Sidenav.module.scss';
 
-function Sidenav(): React.ReactElement {
+export function Sidenav(): React.ReactElement {
+  const router = useRouter();
+
   return (
-    <nav className="Sidenav">
+    <nav className={style.Sidenav}>
       <ul>
         <li>
-          <NavLink exact activeClassName="active-link" to="/">
-            home
-          </NavLink>
+          {/* <NavLink exact activeClassName="active-link" to="/"> */}
+          <Link href="/">
+            <a className={router.pathname == '/' ? style['active-link'] : ''}>
+              home
+            </a>
+          </Link>
+          {/* </NavLink> */}
         </li>
         <li>
-          <NavLink activeClassName="active-link" to="/logbook">
-            logbook
-          </NavLink>
+          {/* <NavLink activeClassName="active-link" to="/logbook"> */}
+          <Link href="/logbook">
+            <a
+              className={
+                router.pathname == '/logbook' ? style['active-link'] : ''
+              }
+            >
+              logbook
+            </a>
+          </Link>
+          {/* </NavLink> */}
         </li>
         <li>
-          <NavLink activeClassName="active-link" to="/developer">
-            developer
-          </NavLink>
+          {/* <NavLink activeClassName="active-link" to="/developer"> */}
+          <Link href="/developer">
+            <a
+              className={
+                router.pathname == '/developer' ? style['active-link'] : ''
+              }
+            >
+              developer
+            </a>
+          </Link>
+          {/* </NavLink> */}
         </li>
         <li>
-          <NavLink activeClassName="active-link" to="/writer">
-            writer
-          </NavLink>
+          {/* <NavLink activeClassName="active-link" to="/writer"> */}
+          <Link href="/writer">
+            <a
+              className={
+                router.pathname == '/writer' ? style['active-link'] : ''
+              }
+            >
+              writer
+            </a>
+          </Link>
+          {/* </NavLink> */}
         </li>
         <li>
-          <NavLink activeClassName="active-link" to="/drummer">
-            drummer
-          </NavLink>
+          {/* <NavLink activeClassName="active-link" to="/drummer"> */}
+          <Link href="/drummer">
+            <a
+              className={
+                router.pathname == '/drummer' ? style['active-link'] : ''
+              }
+            >
+              drummer
+            </a>
+          </Link>
+          {/* </NavLink> */}
         </li>
       </ul>
     </nav>
-  )
+  );
 }
-
-export default Sidenav
