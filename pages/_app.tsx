0380f697b032b298193db5756dcfba4866d810f6
SplinterDev <fabriciojcmontenegro@gmail.com>
2022-04-15 18:34:37 -0700

    Fix ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,11 @@ import { useEffect, useState } from 'react';
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
-import ReactGA from 'react-ga';
 
 import { TopNav } from '../src/components';
 import styles from '../styles/app.module.scss';
 import '../styles/globals.css';
-
-const TRACKING_CODE = 'UA-165360075-1';
-
-ReactGA.initialize(TRACKING_CODE);
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const { route } = useRouter();
@@ -29,8 +25,20 @@ function MyApp({ Component, pageProps }: AppProps) {
           content="Developer, writer, drummer, and all-around cool guy."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+
         <title>FJCMontenegro</title>
       </Head>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=UA-165360075-1"
+      />
+      <Script id="gtag">
+        {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-165360075-1');`}
+      </Script>
       <header>{showNav && <TopNav />}</header>
       <main>
         <Component />
