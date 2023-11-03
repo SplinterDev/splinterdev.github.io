cadf1bf2b311d743b5d050ba7fccb3851299137b
Fabrício Montenegro <fabriciojcmontenegro@gmail.com>
2023-11-03 13:48:32 -0700

    Turn table into grid

--- tkl/src/components/ScheduleTable.tsx
+++ tkl/src/components/ScheduleTable.tsx
@@ -2,7 +2,22 @@ import dayjs from 'dayjs';
 import { getSlotsForCurrentWeek } from '../utils/date';
 import { useState } from 'react';
 import { Button } from './Button';
-import { Row } from './Layout';
+import { Column, Row } from './Layout';
+import styled from 'styled-components';
+
+const Heading = styled.div`
+  font-weight: bold;
+`;
+
+const TodayHeading = styled(Heading)`
+  color: blue;
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(8, 1fr);
+  gap: 10px;
+`;
 
 const ScheduleTable = () => {
   const [weekOffset, setWeekOffset] = useState(0);
@@ -19,17 +34,27 @@ const ScheduleTable = () => {
         <Button onClick={() => setWeekOffset(0)}>Current Week</Button>
         <Button onClick={() => setWeekOffset(weekOffset + 1)}>Next Week</Button>
       </Row>
-      <Row justifyContent="space-between" gap="10px">
-        <div>
-          <div>Time</div>
-          {slots[0].map((time) => {
-            return <div>{dayjs(time).format('HH A')}</div>;
-          })}
-        </div>
+
+      {/* table */}
+
+      <Grid>
+        <Heading>Time</Heading>
         {slots.map((day) => {
-          return <div>{dayjs(day[0]).format('dd (D/MMM)')}</div>;
+          const isToday = dayjs(day[0]).isSame(dayjs(), 'day');
+          const DayHeading = isToday ? TodayHeading : Heading;
+          return <DayHeading>{dayjs(day[0]).format('dd (D/MMM)')}</DayHeading>;
         })}
-      </Row>
+        {slots[0].map((time, index) => {
+          return (
+            <>
+              <div>{dayjs(time).format('h A')}</div>
+              {slots.map((day) => (
+                <div>{/* Your data here */}</div>
+              ))}
+            </>
+          );
+        })}
+      </Grid>
     </div>
   );
 };
